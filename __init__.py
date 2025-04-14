1f030802a4ec09afd93794da2e3eded6f5076416
gordon123 <thegto_2001@yahoo.com>
2025-04-14 05:12:58 +0200

    update

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from .srt2speech import SRT2SpeechNode
+  from .srt2speech import SRT2SpeechNode
 
-NODE_CLASS_MAPPINGS = {
-    "SRT2SpeechNode": SRT2SpeechNode
-}
+  NODE_CLASS_MAPPINGS = {
+      "SRT2SpeechNode": SRT2SpeechNode
+  }
 
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "SRT2SpeechNode": "SRT to Speech"
-}
+  NODE_DISPLAY_NAME_MAPPINGS = {
+      "SRT2SpeechNode": "SRT to Speech"
+  }
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+  __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
