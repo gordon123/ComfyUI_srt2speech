a0de9cb0ceec5659586b19e861c33954f3e20951
gordon123 <thegto_2001@yahoo.com>
2025-04-15 02:31:45 +0200

    update name node

--- __init__.py
+++ __init__.py
@@ -1,9 +1,21 @@
 from .SRT2SpeechNode import SRT2SpeechNode
+from .SplitSRTSentencesNode import SplitSRTSentencesNode
+from .SplitAllSubtitlesNode import SplitAllSubtitlesNode
+from .SubtitleIteratorNode import SubtitleIteratorNode
+from .SaveWavNode import SaveWAVNode
 
 NODE_CLASS_MAPPINGS = {
-    "SRT2SpeechNode": SRT2SpeechNode
+    "SRT2SpeechNode": SRT2SpeechNode,
+    "SplitSRTSentencesNode": SplitSRTSentencesNode,
+    "SplitAllSubtitlesNode": SplitAllSubtitlesNode,
+    "SubtitleIteratorNode": SubtitleIteratorNode,
+    "SaveWAVNode": SaveWAVNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SRT2SpeechNode": "SRT to Speech"
+    "SRT2SpeechNode": "SRT to Speech",
+    "SplitSRTSentencesNode": "Split SRT Sentence (1st)",
+    "SplitAllSubtitlesNode": "Split All Subtitles",
+    "SubtitleIteratorNode": "Subtitle Iterator",
+    "SaveWAVNode": "Save WAV from TTS",
 }
