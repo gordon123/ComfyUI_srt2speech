14fa5cc8aef50464b6e76eb2dd90996efb850e13
gordon123 <thegto_2001@yahoo.com>
2025-04-18 02:34:40 +0200

    ini

--- __init__.py
+++ __init__.py
@@ -2,12 +2,14 @@ from .SaveWavNode import SaveWavNode
 from .GetSubtitleByIndex import GetSubtitleByIndex
 from .MergeWithDummySilence import MergeWithDummySilence
 from .MergeSavedWavWithDummy import MergeSavedWavWithDummy
+from .LoadMergedAudio import LoadMergedAudio
 
 NODE_CLASS_MAPPINGS = {
     "SaveWavNode": SaveWavNode,
     "GetSubtitleByIndex": GetSubtitleByIndex,
     "MergeWithDummySilence": MergeWithDummySilence,
     "MergeSavedWavWithDummy": MergeSavedWavWithDummy,
+    "LoadMergedAudio": LoadMergedAudio,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -15,6 +17,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "GetSubtitleByIndex": "Get Subtitle By Index",
     "MergeWithDummySilence": "Merge with Dummy Silence",
     "MergeSavedWavWithDummy": "Merge Saved Wav with Dummy",
+    "LoadMergedAudio": "Load Merged Audio",
 }
 
 WEB_DIRECTORY = "./web"
