4e0f55be59b247ca3c9230fb7df38ffa1af9f761
gordon123 <thegto_2001@yahoo.com>
2025-04-19 19:49:55 +0200

    bak unfinished files

--- __init__.py
+++ __init__.py
@@ -1,26 +1,26 @@
 from .SaveWavNode import SaveWavNode
 from .GetSubtitleByIndex import GetSubtitleByIndex
-from .MergeWithDummySilence import MergeWithDummySilence
-from .MergeSavedWavWithDummy import MergeSavedWavWithDummy
-from .LoadMergedAudio import LoadMergedAudio
-from .GetSubtitleIndexByFilename import GetSubtitleIndexByFilename
+# from .MergeWithDummySilence import MergeWithDummySilence
+# from .MergeSavedWavWithDummy import MergeSavedWavWithDummy
+# from .LoadMergedAudio import LoadMergedAudio
+# from .GetSubtitleIndexByFilename import GetSubtitleIndexByFilename
 
 NODE_CLASS_MAPPINGS = {
     "SaveWavNode": SaveWavNode,
     "GetSubtitleByIndex": GetSubtitleByIndex,
-    "MergeWithDummySilence": MergeWithDummySilence,
-    "MergeSavedWavWithDummy": MergeSavedWavWithDummy,
-    "LoadMergedAudio": LoadMergedAudio,
-    "GetSubtitleIndexByFilename": GetSubtitleIndexByFilename,
+#    "MergeWithDummySilence": MergeWithDummySilence,
+#    "MergeSavedWavWithDummy": MergeSavedWavWithDummy,
+#    "LoadMergedAudio": LoadMergedAudio,
+#    "GetSubtitleIndexByFilename": GetSubtitleIndexByFilename,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SaveWavNode": "Save Wav from TTS",
     "GetSubtitleByIndex": "Get Subtitle By Index",
-    "MergeWithDummySilence": "Merge with Dummy Silence",
-    "MergeSavedWavWithDummy": "Merge Saved Wav with Dummy",
-    "LoadMergedAudio": "Load Merged Audio",
-    "GetSubtitleIndexByFilename": "Get Subtitle Index By Filename",
+ #   "MergeWithDummySilence": "Merge with Dummy Silence",
+ #   "MergeSavedWavWithDummy": "Merge Saved Wav with Dummy",
+ #   "LoadMergedAudio": "Load Merged Audio",
+ #   "GetSubtitleIndexByFilename": "Get Subtitle Index By Filename",
 }
 
 WEB_DIRECTORY = "./web"
